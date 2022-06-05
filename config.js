18308e81c33673b460ee45b587c1b50d71319e0d
Thaoyeuduyen <106911520+Thaoyeuduyen@users.noreply.github.com>
2022-06-05 18:10:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi vợ duyên",
+    introTitle: 'cj iu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim cj lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    title: 'Phải chăng em đã yêu ngay từ tin nhắn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnYes: 'Yêu em 🥰 <33',
+    btnNo: 'Không bao giờ :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cj lại yêu e <3',
+    btnReply: 'Gửi cho e <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Em biết mà 🥰. Yêu cj nhiều nhiều 😘😘',
+    messDesc: 'E sẽ yêu mỗi cj thôi, Yêu cj mãiiii.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    
 }
